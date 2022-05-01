f85e1c1c00a12e3d94a9d187ccc0e2dfc9460a79
chebread <83535893+chebread@users.noreply.github.com>
2022-05-02 03:13:37 +0900

    feat: basics init

--- src/app.js
+++ src/app.js
@@ -1,38 +1,101 @@
 import './reset.css';
 import './app.css';
+import './components/header.css';
+import './components/content.css';
 import logoImg from './imgs/logo.png';
+import { renderHTML } from './components/renderHTML.js';
 
-const root = document.querySelector('#root');
-document.title = 'colvier';
-const render = () => {
-  root.innerHTML = `
-    <header class="header">
-      <div class="header-wrapper">
-        <div class="header-title">
-          <a href="/"><img src="${logoImg}"/></a>
+const app = () => {
+  const render = () => {
+    const contentMsg = `
+      <header class="header">
+        <div class="header-wrapper">
+          <div class="header-title">
+            <a href="/"><img src="${logoImg}" /></a>
+          </div>
+        </div>
+      </header>
+      <div class="content">
+        <div class="content-wrapper">
+          ${['R', 'G', 'B']
+            .map(
+              (item, key) =>
+                `<div class="content-items" id="${item}"><input value="" placeholder="${item}" tabindex="${
+                  key + 1
+                }" type="text" maxlength="3" /></div>`
+            )
+            .join('')}
+        </div>
+        <div class="content-wrapper2">
+          <div class="content-items2">
+            <div class="content-viewer-name" id="viewer-name"></div>
+          </div>
+          <div class="content-items2">
+            <div class="content-viewer-color" id="viewer-color"></div>
+          </div>
         </div>
       </div>
-    </header>
-    <div class="container">
-      <div class="container-select">
-        <button id="select" value="rgb">RGB</button>
-        <button id="select" value="hash">Hash</button>
-      </div>
-      <div class="container-content"></div>
-    </div>
-    <footer class="footer">
-    </footer>
-  `;
-  document.querySelectorAll('#select').forEach(item => {
-    item.addEventListener('click', e => {
-      if (e.target.value === 'rgb') {
-        document.querySelector('.container-content').innerHTML = `
-          <input type="number"/>
-        `;
-      } else {
-        document.querySelector('.container-content').innerHTML = `hellojh`;
-      }
+    `;
+    renderHTML(contentMsg, document.querySelector('#root'));
+    document.querySelectorAll('.content-items').forEach(item => {
+      item.addEventListener('change', e => {
+        if (
+          isNaN(e.target.value) === false &&
+          e.target.value >= 0 &&
+          e.target.value <= 255
+        ) {
+          // console.log(Number(e.target.value));
+          e.target.attributes.value.value = e.target.value;
+          if (
+            (document.querySelector('#R').attributes.class.ownerElement
+              .childNodes[0].attributes.value.value ===
+              '') ===
+              false &&
+            (document.querySelector('#G').attributes.class.ownerElement
+              .childNodes[0].attributes.value.value ===
+              '') ===
+              false &&
+            (document.querySelector('#B').attributes.class.ownerElement
+              .childNodes[0].attributes.value.value ===
+              '') ===
+              false
+          ) {
+            // for viewer-name: rgb name change (renderHTML used)
+            renderHTML(
+              `rgb(${
+                document.querySelector('#R').attributes.class.ownerElement
+                  .childNodes[0].attributes.value.value
+              }, ${
+                document.querySelector('#G').attributes.class.ownerElement
+                  .childNodes[0].attributes.value.value
+              }, ${
+                document.querySelector('#B').attributes.class.ownerElement
+                  .childNodes[0].attributes.value.value
+              })`,
+              document.querySelector('#viewer-name')
+            );
+            // for viewer-color: color change (style used)
+            document.querySelector(
+              '#viewer-color'
+            ).style = `background-color: rgb(${
+              document.querySelector('#R').attributes.class.ownerElement
+                .childNodes[0].attributes.value.value
+            }, ${
+              document.querySelector('#G').attributes.class.ownerElement
+                .childNodes[0].attributes.value.value
+            }, ${
+              document.querySelector('#B').attributes.class.ownerElement
+                .childNodes[0].attributes.value.value
+            })`;
+          }
+        } else {
+          e.target.value = '';
+          e.target.placeholder =
+            'Enter a positive number less than or equal to 255';
+        }
+      });
     });
-  });
+  };
+  render();
 };
-render();
+app();
